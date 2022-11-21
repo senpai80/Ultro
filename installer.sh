cfe31fb98bc3a5065566a871791e7f49a5909bc8
senpai80 <73426077+senpai80@users.noreply.github.com>
2022-11-21 23:34:15 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-REPO="https://github.com/TeamUltroid/Ultroid.git"
-DIR="/root/TeamUltroid"
+REPO="https://github.com/senpai80/Ultro.git"
+DIR="/root/senpai80"
 
 spinner(){
     local pid=$!
@@ -36,7 +36,7 @@ clone_repo(){
             fi
             return
     fi
-    echo -e "Cloning Ultroid ${BRANCH}... "
+    echo -e "Cloning Ultro ${BRANCH}... "
     git clone -b $BRANCH $REPO $DIR
 }
 
